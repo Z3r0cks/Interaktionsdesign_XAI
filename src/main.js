a94f0ec697366ce1ed5e65fb8b901dbb78a2e434
Z3r0cks <48802394+Z3r0cks@users.noreply.github.com>
2023-11-10 15:48:50 +0100

    WIP:  connection

--- src/main.js
+++ src/main.js
@@ -70,14 +70,12 @@ function generateVisualNN(input, hidden, output) {
    outputNodes = [];
 
    let i = 0;
-   // Trainieren Sie das Netzwerk mit Ihren Daten
-   // nn2.feedforward(inputArray);
    // TODO: ADD Data to inputNodes (simple_NN.js)
-   generateNodes(input, -2, 0xee6363, NN.inputNodes, inputNodes);
+   generateNodes(input, -1, 0xee6363, NN.inputNodes, inputNodes);
    for (i; i < hidden.length; i++) {
-      generateNodes(hidden[i], i + 2, 0x58ea8e, NN.hiddenNodes[i], hiddenNodes);
+      generateNodes(hidden[i], i, 0x58ea8e, NN.hiddenNodes[i], hiddenNodes);
    }
-   generateNodes(output, i + 2, 0x4f6357, NN.biasOutput.data, outputNodes);
+   generateNodes(output, i, 0x4f6357, NN.biasOutput.data, outputNodes);
    neuralNodes = [input, hidden, output];
    const cameraZoom = Math.max(input, hidden, output) * sphereDistance / 4;
    cameraZ.value = cameraZoom < 1 ? 8 : 8 * cameraZoom;
@@ -97,9 +95,111 @@ function generateNodes(nodesCount, pos, color, data, nodeArray) {
    }
 }
 
-function createNeuralNodes(input, hidden, output) {
-   neuralNodes = [input, hidden, output];
+function haveNodesChanged() {
+   if (NN === undefined) return false;
+   if (neuralNodes.length <= 0) return false;
+   const currentHiddenLayerValues = getAllHiddenLayer();
+   // Vergleichen Sie die Länge der Arrays, um sicherzustellen, dass sie die gleiche Anzahl von Schichten haben.
+   if (neuralNodes[1].length !== currentHiddenLayerValues.length) return true;
+
+   // Überprüfen Sie, ob die Werte der einzelnen versteckten Schichten sich geändert haben.
+   for (let i = 0; i < neuralNodes[1].length; i++) {
+      if (neuralNodes[1][i] != currentHiddenLayerValues[i]) return true;
+   }
+
+   // Überprüfen Sie, ob die Werte für die Eingabe- und Ausgabeknoten sich geändert haben.
+   return inputInput.value != neuralNodes[0] || outputInput.value != neuralNodes[2];
 }
+
+function animate() {
+   if (haveNodesChanged()) {
+      generateVisualNN(inputInput.value, getAllHiddenLayer(), outputInput.value);
+      if (connBox.checked) {
+         // Verbinde Eingabeschicht mit der ersten versteckten Schicht
+         // console.log("inputNodes");
+         // console.log(inputNodes);
+         // console.log("hiddenNodes[0]");
+         // console.log(hiddenNodes);
+         connectNodes(inputNodes, hiddenNodes[0], 0xaaaaaa, "input");
+         // Verbinde alle versteckten Schichten miteinander
+         for (let i = 0; i < hiddenNodes.length - 1; i++) {
+            connectNodes(hiddenNodes[i], hiddenNodes[i + 1], 0xaaaaaa, "hidden");
+         }
+         // Verbinde letzte versteckte Schicht mit Ausgabeschicht
+         connectNodes(hiddenNodes[hiddenNodes.length - 1], outputNodes, 0xaaaaaa, "output");
+      }
+   }
+   camera.position.x = cameraX.value;
+   camera.position.y = cameraY.value;
+   camera.position.z = cameraZ.value;
+   requestAnimationFrame(animate);
+   renderer.render(scene, camera);
+}
+
+animate();
+
+function connectNodes(nodeArray1, nodeArray2, color, sequenz) {
+   // console.log("node1");
+   // console.log(nodeArray1);
+   // console.log("node2");
+   // console.log(nodeArray2);
+   // console.log("________________________");
+   if (sequenz == "input") {
+      for (let i = 0; i < nodeArray1.length; i++) {
+         // for (let j = 0; j < nodeArray2.length; j++) {
+         drawLine(nodeArray1[i], nodeArray2, color);
+         // }
+      }
+      return;
+   }
+
+   if (sequenz == "hidden") {
+      for (let i = 0; i < nodeArray1.length; i++) {
+         for (let j = 0; j < array.length[i]; j++) {
+            for (let k = 0; k < array.length[i]; k++) {
+               drawLine(nodeArray1[i][j], nodeArray2[i][k], color);
+            }
+         }
+      }
+      return;
+   }
+
+   if (sequenz == "output") {
+      for (let i = 0; i < nodeArray1.length; i++) {
+         for (let j = 0; j < nodeArray2.length; j++) {
+            drawLine(nodeArray1[i], nodeArray2[j], color);
+         }
+      }
+      return;
+   }
+}
+
+// Funktion, um eine Linie zwischen zwei Knoten zu zeichnen
+function drawLine(startNode, endNode, color = 0xffffff) {
+   // Erstellen einer Vektorreihe, die die Positionen der Knoten enthält
+   const points = [];
+   points.push(startNode.position.clone());
+   points.push(endNode.position.clone());
+
+   // Erstellen einer Liniengeometrie aus den Punkten
+   const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+   // Erstellen eines Materials für die Linie
+   const material = new THREE.LineBasicMaterial({ color: color });
+
+   // Erstellen einer Linie mit der definierten Geometrie und dem Material
+   const line = new THREE.Line(geometry, material);
+   line.renderOrder = -1;
+   // Hinzufügen der Linie zur Szene
+   scene.add(line);
+
+   // Rückgabe der erstellten Linie, falls benötigt
+   return line;
+}
+
+// function createNeuralNodes(input, hidden, output) {
+//    neuralNodes = [input, hidden, output];
+// }
 
 
 // function showNeuronTooltip(neuronData, position) {
@@ -168,74 +268,3 @@ function createNeuralNodes(input, hidden, output) {
 //    }
 // });
 
-function haveNodesChanged() {
-   if (NN === undefined) return false;
-   if (neuralNodes.length <= 0) return false;
-
-   const currentHiddenLayerValues = getAllHiddenLayer();
-   // Vergleichen Sie die Länge der Arrays, um sicherzustellen, dass sie die gleiche Anzahl von Schichten haben.
-   if (neuralNodes[1].length !== currentHiddenLayerValues.length) return true;
-
-   // Überprüfen Sie, ob die Werte der einzelnen versteckten Schichten sich geändert haben.
-   for (let i = 0; i < neuralNodes[1].length; i++) {
-      if (neuralNodes[1][i] != currentHiddenLayerValues[i]) return true;
-   }
-
-   // Überprüfen Sie, ob die Werte für die Eingabe- und Ausgabeknoten sich geändert haben.
-   return inputInput.value != neuralNodes[0] || outputInput.value != neuralNodes[2];
-}
-
-function animate() {
-   if (haveNodesChanged()) {
-      generateVisualNN(inputInput.value, getAllHiddenLayer(), outputInput.value);
-      if (connBox.checked) {
-         console.log("test");
-         // Verbinde Eingabeschicht mit der ersten versteckten Schicht
-         connectNodes(inputNodes, hiddenNodes[0], 0xaaaaaa);
-         // Verbinde alle versteckten Schichten miteinander
-         for (let i = 0; i < hiddenNodes.length - 1; i++) {
-            connectNodes(hiddenNodes[i], hiddenNodes[i + 1], 0xaaaaaa);
-         }
-         // Verbinde letzte versteckte Schicht mit Ausgabeschicht
-         connectNodes(hiddenNodes[hiddenNodes.length - 1], outputNodes, 0xaaaaaa);
-      }
-   }
-   camera.position.x = cameraX.value;
-   camera.position.y = cameraY.value;
-   camera.position.z = cameraZ.value;
-   requestAnimationFrame(animate);
-   renderer.render(scene, camera);
-}
-
-animate();
-
-function connectNodes(nodeArray1, nodeArray2, color) {
-   for (let i = 0; i < nodeArray1.length; i++) {
-      for (let j = 0; j < nodeArray2.length; j++) {
-         drawLine(nodeArray1[i], nodeArray2[j], color);
-      }
-   }
-}
-
-// Funktion, um eine Linie zwischen zwei Knoten zu zeichnen
-function drawLine(startNode, endNode, color = 0xffffff) {
-   // Erstellen einer Vektorreihe, die die Positionen der Knoten enthält
-   const points = [];
-   points.push(startNode.position.clone());
-   points.push(endNode.position.clone());
-
-   // Erstellen einer Liniengeometrie aus den Punkten
-   const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-   // Erstellen eines Materials für die Linie
-   const material = new THREE.LineBasicMaterial({ color: color });
-
-   // Erstellen einer Linie mit der definierten Geometrie und dem Material
-   const line = new THREE.Line(geometry, material);
-   line.renderOrder = -1;
-   // Hinzufügen der Linie zur Szene
-   scene.add(line);
-
-   // Rückgabe der erstellten Linie, falls benötigt
-   return line;
-}
